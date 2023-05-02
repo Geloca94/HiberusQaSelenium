fc67f717584235b193224c6eba8b639239fbb9ec
Gelo <gelogarcia94@gmail.com>
2023-05-02 02:00:47 +0200

    Inventario completado

--- Ejercicio1/EjerciciosQA/src/test/java/EjercicioSeleniumJunit/Inventario/Inventario.java
+++ Ejercicio1/EjerciciosQA/src/test/java/EjercicioSeleniumJunit/Inventario/Inventario.java
@@ -14,7 +14,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import EjercicioSeleniumJunit.Login.LoginAceso;
 
 
+import java.sql.SQLOutput;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Inventario {
@@ -63,19 +65,29 @@ public class Inventario {
 
     }
     //@Test
-    @Ignore
+    @Test
     public void validationNumeroResultados(){
 
         //Paso 1
         List<WebElement> elementos = driver.findElements(By.xpath("//div[@class='inventory_item']"));
-        int cantidad = elementos.size();
-        int cantidadEsperada = 6;
 
         //Paso 2
-        Assert.assertEquals("ERROR: La cantidad no corresponde con la esperada:" , cantidadEsperada,cantidad);
+        try{
+
+            int cantidad = elementos.size();
+            int cantidadEsperada = 6;
+
+            Assert.assertEquals( cantidadEsperada,cantidad);
+            System.out.println("La cantidad esperada es: " + cantidadEsperada);
+            System.out.println("La cantidad en el carrito obtenida es : " + cantidad);
+        } catch (AssertionError e){
+            System.out.println("La cantidad es incorrecta, cantidad obtenida es incorrecta " );
+        }
+
+
 
     }
-    @Ignore
+    @Test
     public void validationProducto(){
 
         boolean isPresent = false;
@@ -100,7 +112,6 @@ public class Inventario {
 
     }
 
-
     @Test
     public void addCarrito(){
 
@@ -111,13 +122,21 @@ public class Inventario {
         //Paso 2
         String carrito = driver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).getText();
 
-        //Paso 3
-        String carritoCorrecto = "1";
 
-        //Paso 4
 
-        Assert.assertEquals("ERROR: La cantidad no corresponde con la esperada:" , carritoCorrecto,carrito);
 
+
+        try{
+
+            //Paso 3
+            String carritoCorrecto = "1";
+
+            Assert.assertEquals( carritoCorrecto,carrito);
+            System.out.println("La cantidad esperada es: " + carritoCorrecto);
+            System.out.println("La cantidad en el carrito obtenida es : " + carrito);
+        } catch (AssertionError e){
+            System.out.println("La cantidad es incorrecta, cantidad obtenida: " + carrito);
+        }
 
 
     }
@@ -141,17 +160,156 @@ public class Inventario {
 
             String carritoCorrecto = "0";
 
-            //Paso 4
+            Assert.assertEquals( carritoCorrecto,carrito);
+            System.out.println("La cantidad esperada es: " + carritoCorrecto);
+            System.out.println("La cantidad en el carrito obtenida es : " + carrito);
+        } catch (AssertionError e){
+            System.out.println("La cantidad es incorrecta, cantidad obtenida: " + carrito);
+        }
 
-            Assert.assertEquals("ERROR: La cantidad no corresponde con la esperada:" , carritoCorrecto,carrito);
-        } catch (TimeoutException timeoutException){
 
+
+
+
+    }
+
+    @Test
+    public void add3Carrito(){
+
+        //Paso 1
+        WebElement buttonAdd = driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']"));
+        buttonAdd.click();
+
+        WebElement buttonAdd2 = driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-fleece-jacket']"));
+        buttonAdd2.click();
+
+        WebElement buttonAdd3 = driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']"));
+        buttonAdd3.click();
+
+
+        String carrito = driver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).getText();
+
+
+        try{
+
+            //Paso 2
+            String carritoCorrecto = "3";
+
+            Assert.assertEquals( carritoCorrecto,carrito);
+            System.out.println("La cantidad esperada es: " + carritoCorrecto);
+            System.out.println("La cantidad en el carrito obtenida es : " + carrito);
+        } catch (AssertionError e){
+            System.out.println("La cantidad es incorrecta, cantidad obtenida: " + carrito);
+
+        }
+
+        /*
+        int i = 0, cantidad = 3, rango = 6;
+        int arreglo[] = new int[cantidad];
+
+        arreglo[i] = (int) (Math.random()*rango);
+
+        for(i=1; i<cantidad; i++) {
+            arreglo[i] = (int) (Math.random()*rango);
+            for (int j = 0; j<1; j++){
+                //Si el valor es repetido retrocede hasta aque el valor sea otro diferente
+                if (arreglo[i]==arreglo[j]){
+                    i--;
+                }
+            }
+        }
+
+        for (int k=0; k<cantidad ;k++){
+
+            //Para imprimir la cantidad
+        }
+        */
+    }
+
+    @Test
+    public void alphabetic(){
+
+        //Paso 1
+        WebElement  filtro = driver.findElement(By.xpath("//select[@data-test='product_sort_container']"));
+        filtro.click();
+        WebElement  opction = driver.findElement(By.xpath("//option[@value='az']"));
+        opction.click();
+
+        WebElement spam = driver.findElement(By.xpath("//span[@class='active_option']"));
+        String texto = spam.getAttribute("textContent");
+
+        String textoEsperado = "Name (A to Z)";
+
+        //Paso2
+
+        try{
+            Assert.assertEquals(textoEsperado,texto);
+            System.out.println("El orden de los productos es el correcto: " + texto);
+        }catch (AssertionError e){
+            System.out.println("ERROR: El orden de los productos es incorrecto. El texto obtenido es: " + texto );
 
         }
 
 
 
     }
+
+    @Test
+    public void lowHigh(){
+
+        //Paso 1
+      WebElement  filtro = driver.findElement(By.xpath("//select[@data-test='product_sort_container']"));
+      filtro.click();
+      WebElement  opction = driver.findElement(By.xpath("//option[@value='lohi']"));
+      opction.click();
+
+      WebElement spam = driver.findElement(By.xpath("//span[@class='active_option']"));
+      String texto = spam.getAttribute("textContent");
+
+      String textoEsperado = "Price (low to high)";
+
+        //Paso2
+
+        try{
+            Assert.assertEquals(textoEsperado,texto);
+            System.out.println("El orden de los productos es el correcto: " + texto);
+        }catch (AssertionError e){
+            System.out.println("ERROR: El orden de los productos es incorrecto. El texto obtenido es: " + texto );
+
+        }
+
+
+
+    }
+
+    @Test
+    public void highLow(){
+
+        //Paso 1
+        WebElement  filtro = driver.findElement(By.xpath("//select[@data-test='product_sort_container']"));
+        filtro.click();
+        WebElement  opction = driver.findElement(By.xpath("//option[@value='hilo']"));
+        opction.click();
+
+        WebElement spam = driver.findElement(By.xpath("//span[@class='active_option']"));
+        String texto = spam.getAttribute("textContent");
+
+        String textoEsperado = "Price (high to low)";
+
+        //Paso2
+
+        try{
+            Assert.assertEquals(textoEsperado,texto);
+            System.out.println("El orden de los productos es el correcto: " + texto);
+        }catch (AssertionError e){
+            System.out.println("ERROR: El orden de los productos es incorrecto. El texto obtenido es: " + texto );
+
+        }
+
+
+
+    }
+
     @After
     public void tearDown(){
 
